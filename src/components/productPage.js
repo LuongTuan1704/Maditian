2524212f6ccbdb9d7ef3c3db582d6c11e5d7f655
LuongTuan <62272226+LuongTuan1704@users.noreply.github.com>
2022-04-25 19:46:48 +0700

    add banner

--- src/components/productPage.js
+++ src/components/productPage.js
@@ -1,50 +1,34 @@
+import Slider from 'react-slick'
+import banner1 from './../images/banner_0.png'
+import banner2 from './../images/banner_1.png'
 import { Products as products } from './../datas/ProductsData'
 import { CartPlus } from "react-bootstrap-icons"
 import { Link } from "react-router-dom"
+import RenderProducts from './renderProducts'
 export default function ProductPage({ children }) {
+    let settings = {
+        dots: false,
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        pauseOnHover: false,
+        speed: 2000,
+        autoplaySpeed: 5000,
+
+    };
     document.title = 'Sản phẩm'
     return (
         children ? { children } :
-            <div className='pt-10 bg-red-700'>
-                <h1 className='relative max-w-2xl mx-auto text-center font-bold text-2xl text-white  border-b-2 border-gray-700' >
+            <div className='bg-red-700'>
+                <Slider {...settings}>
+                    <img className="block cursor-pointer h-screen" src={banner1} alt='banner0' />
+                    <img className="block cursor-pointer h-screen" src={banner2} alt='banner1' />
+                </Slider>
+                <h1 className='relative mt-32 max-w-2xl mx-auto text-center font-bold text-2xl text-white  border-b-2 border-gray-700' >
                     <span className='absolute max-w-2xl mx-auto font-bold bg-red-700 p-3 z-10 left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 uppercase'>sản phẩm</span>
                 </h1>
-                <div className="product-wrapper max-w-screen-xl mx-auto  py-28 px-6 grid grid-cols-4 gap-6 select-none">
-
-                    {
-                        products.map((product, i) => {
-                            return (
-                                <Link key={product.id} to={`./../san-pham/id=${product.id}`}
-                                    className="product-item block  transition-transform bg-white hover:scale-105 p-3 hover:border rounded-lg ">
-                                    <div className="flex flex-col h-full">
-                                        <div className='h-1/2 flex flex-col justify-center'>
-                                            {
-                                                product.imgURL ?
-                                                    <img className='block mx-auto rounded-md' src={`${product.imgURL}`} width={150} alt='product item' /> :
-                                                    <div className="h-80 bg-blue-400 mb-2 rounded-sm">
-
-                                                    </div>
-                                            }</div>
-                                        <div className='mt-auto py-6 px-3 flex flex-col items-center space-y-4'>
-                                            <div className='flex-grow text-center'>
-                                                <h3 className="prd-prdName font-bold text-xl text-gray-900">{product.name}</h3>
-                                                <p className="prd-moretext text-sm text-gray-800">{product.more}</p>
-                                            </div>
-                                            <div onClick={function (e) { e.preventDefault() }} className="prd-wrrapper cursor-default flex items-center space-x-4 px-4 py-2 bg-gray-300 rounded-xl">
-                                                <div className="text-red-700 text-xl">{product.price}</div>
-                                                <a onClick={function (e) { e.stopPropagation() }} className='block' href={`/them-san-pham-vao-gio-hang/?id-san-pham=${product.id}`}>
-                                                    <CartPlus size={32} className="transition-colors duration-300 hover:text-blue-700" />
-                                                </a>
-
-                                            </div>
-                                        </div>
-                                    </div>
-                                </Link>
-                            )
-                        })
-                    }
-
-                </div>
+                <RenderProducts datas={products} />
             </div>
     )
 }
